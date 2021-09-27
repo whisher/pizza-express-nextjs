b47f5a92a5d7c05b8be583b32ef0bdeff4f3b046
Fabio-Bedini <io@ilwebdifabio.it>
2021-09-27 22:53:15 +0200

    add prisma models and api

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/chi-siamo":["static\u002Fchunks\u002Fpages\u002Fchi-siamo.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fchi-siamo"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
