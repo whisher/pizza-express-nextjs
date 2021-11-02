5437474daec374dba8deb32313de89f47a7360ac
Fabio Bedini aka whisher <io@ilwebdifabio.it>
2021-11-02 20:48:01 +0100

    test vercel

--- app/util/prisma.ts
+++ app/util/prisma.ts
@@ -7,7 +7,7 @@ declare global {
 const prisma =
   global.prisma ||
   new PrismaClient({
-    log: ["query"]
+    log: ["query", "error", "info", "warn"]
   });
 
 if (process.env.NODE_ENV !== "production") {
