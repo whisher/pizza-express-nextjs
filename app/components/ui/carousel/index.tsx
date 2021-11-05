88936a13eafd3983af2106d1f531c7d530200dd4
Fabio Bedini aka whisher <io@ilwebdifabio.it>
2021-11-05 21:30:04 +0100

    next image for carousel

--- app/components/ui/carousel/index.tsx
+++ app/components/ui/carousel/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NextLink from "next/link";
-import { Box, Image, Link, useBreakpointValue } from "@chakra-ui/react";
+import Image from "next/image";
+import { Box, LinkOverlay, useBreakpointValue } from "@chakra-ui/react";
 import { CarouselMain } from "./main";
 
 const Carousel = () => {
@@ -10,85 +11,109 @@ const Carousel = () => {
     <Box mt="1rem">
       <CarouselMain numberToShow={3}>
         <NextLink href={"/shop/pizza-ai-funghi"} passHref>
-          <Link>
-            <Image
-              boxSize={width}
-              objectFit="fill"
-              src={`${src}pizza-funghi.min.jpg`}
-              alt="Pizza ai Funghi"
-            />
-          </Link>
+          <LinkOverlay>
+            <Box pos="relative" boxSize={width}>
+              <Image
+                width={width}
+                height={width}
+                layout="fill"
+                src={`${src}pizza-funghi.min.jpg`}
+                alt="Pizza ai Funghi"
+              />
+            </Box>
+          </LinkOverlay>
         </NextLink>
 
         <NextLink href={"/shop/pizza-margherita"} passHref>
-          <Link>
-            <Image
-              boxSize={width}
-              objectFit="fill"
-              src={`${src}pizza-margherita.min.jpg`}
-              alt="Pizza Margherita"
-            />
-          </Link>
+          <LinkOverlay>
+            <Box pos="relative" boxSize={width}>
+              <Image
+                width={width}
+                height={width}
+                layout="fill"
+                src={`${src}pizza-margherita.min.jpg`}
+                alt="Pizza Margherita"
+              />
+            </Box>
+          </LinkOverlay>
         </NextLink>
         <NextLink href={"/shop/pizza-marinara"} passHref>
-          <Link>
-            <Image
-              boxSize={width}
-              objectFit="fill"
-              src={`${src}pizza-marinara.min.jpg`}
-              alt="Pizza Marinara"
-            />
-          </Link>
+          <LinkOverlay>
+            <Box pos="relative" boxSize={width}>
+              <Image
+                width={width}
+                height={width}
+                layout="fill"
+                src={`${src}pizza-marinara.min.jpg`}
+                alt="Pizza Marinara"
+              />
+            </Box>
+          </LinkOverlay>
         </NextLink>
         <NextLink href={"/shop/pizza-quattro-stagioni"} passHref>
-          <Link>
-            <Image
-              boxSize={width}
-              objectFit="fill"
-              src={`${src}pizza-quattro-stagioni.min.jpg`}
-              alt="Pizza Quattro Stagioni"
-            />
-          </Link>
+          <LinkOverlay>
+            <Box pos="relative" boxSize={width}>
+              <Image
+                width={width}
+                height={width}
+                layout="fill"
+                src={`${src}pizza-quattro-stagioni.min.jpg`}
+                alt="Pizza Quattro Stagioni"
+              />
+            </Box>
+          </LinkOverlay>
         </NextLink>
         <NextLink href={"/shop/pizza-amalfi"} passHref>
-          <Link>
-            <Image
-              boxSize={width}
-              objectFit="fill"
-              src={`${src}pizza-amalfi.min.jpg`}
-              alt="Pizza Amalfi"
-            />
-          </Link>
+          <LinkOverlay>
+            <Box pos="relative" boxSize={width}>
+              <Image
+                width={width}
+                height={width}
+                layout="fill"
+                src={`${src}pizza-amalfi.min.jpg`}
+                alt="Pizza Amalfi"
+              />
+            </Box>
+          </LinkOverlay>
         </NextLink>
         <NextLink href={"/shop/pizza-bufala"} passHref>
-          <Link>
-            <Image
-              boxSize={width}
-              objectFit="fill"
-              src={`${src}pizza-bufala.min.jpg`}
-              alt="Pizza con Bufala"
-            />
-          </Link>
+          <LinkOverlay>
+            <Box pos="relative" boxSize={width}>
+              <Image
+                width={width}
+                height={width}
+                layout="fill"
+                src={`${src}pizza-bufala.min.jpg`}
+                alt="Pizza con Bufala"
+              />
+            </Box>
+          </LinkOverlay>
         </NextLink>
         <NextLink href={"/shop/pizza-contadina"} passHref>
-          <Link>
-            <Image
-              boxSize={width}
-              objectFit="fill"
-              src={`${src}pizza-contadina.min.jpg`}
-              alt="Pizza Contadina"
-            />
-          </Link>
+          <LinkOverlay>
+            <Box pos="relative" boxSize={width}>
+              <Image
+                width={width}
+                height={width}
+                layout="fill"
+                src={`${src}pizza-contadina.min.jpg`}
+                alt="Pizza Contadina"
+              />
+            </Box>
+          </LinkOverlay>
         </NextLink>
         <NextLink href={"/shop/pizza-patata"} passHref>
-          <Link>
-            <Image
-              boxSize={width}
-              objectFit="fill"
-              src={`${src}pizza-patata.min.jpg`}
-              alt="Pizza Patata"
-            />
-          </Link>
+          <LinkOverlay>
+            <Box pos="relative" boxSize={width}>
+              <Image
+                width={width}
+                height={width}
+                layout="fill"
+                src={`${src}pizza-patata.min.jpg`}
+                alt="Pizza Patata"
+              />
+            </Box>
+          </LinkOverlay>
         </NextLink>
       </CarouselMain>
     </Box>
