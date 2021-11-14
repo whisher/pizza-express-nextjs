17a4fb17ae6aed18602d2629c59ae845b22c744b
Fabio Bedini aka whisher <io@ilwebdifabio.it>
2021-11-14 12:15:12 +0100

    update

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -7,7 +7,7 @@ function Error({ statusCode }: ErrorProps) {
   return (
     <p>
       {statusCode
-        ? `An error ${statusCode} occurred on server`
+        ? `An big error ${statusCode} occurred on server`
         : "An error occurred on client"}
     </p>
   );
